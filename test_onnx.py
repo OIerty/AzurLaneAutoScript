cce4f5183225d9380a628005a79b87998b69efb7
wess09 <zicheng_1202@outlook.com>
2026-03-19 00:02:28 +0900

    删除测试脚本

--- test_onnx.py
+++
@@ -1,7 +0,0 @@
-import onnxruntime as ort
-
-model_fp = 'bin/cnocr_models/cnocr-v2.3-number-densenet_lite_136-fc-epoch=023.onnx'
-session = ort.InferenceSession(model_fp)
-outputs = session.get_outputs()
-for o in outputs:
-    print(o.name, o.shape)
